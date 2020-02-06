65fffc21caf4d7520f563a0939b5459f376e977e
Joe Gitonga <joe.gitonga@mcmsolutions.co.uk>
2020-02-06 17:32:00 +0300

    In the beginning

---
+++ HLServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace HLServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var ourHl7Server = new OurSimpleMultiThreadedTcpServer();
+            //starting the server
+            ourHl7Server.StartOurTcpServer(1080);
+
+            Console.WriteLine("Press any key to exit program...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+## H7 Protocol
+This project demostrates a simple client server communication using MLLP to send HL7 messages
+
+### API Integration
+There's a need to create an API to integrate the Hemolyzer device to LIMS to facilitate the posting of results.
+
+### Support
+Ideas are greatly welcome on this project. Thank you!
